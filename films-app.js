a6a41e21ed75e3e6819b48dbd622bd07f0d49406
Мария <slinkywizard@mail.ru>
2021-02-21 15:50:04 +0300

    Автоматизация вопросов, если ответы удовлетворяют заданным условиям - занести фильм в базу фильмов, если кол-во фильмов удовлетворяют заданным условиям - выводить сообщения

--- films-app.js
+++ films-app.js
@@ -1,19 +1,74 @@
+// Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+//'Сколько фильмов вы уже посмотрели?'
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотели?", "");
+
+// Создать объект personalMovieDB и в него поместить такие свойства:
+//     - count - сюда передается ответ на первый вопрос
+//     - movies - в это свойство поместить пустой объект
+//     - actors - тоже поместить пустой объект
+//     - genres - сюда поместить пустой массив
+//     - privat - в это свойство поместить boolean(логическое) значение false
+// const personalMovieDB =  {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// Задайте пользователю по два раза вопросы:
+//     - 'Один из последних просмотренных фильмов?'
+//     - 'На сколько оцените его?'
+// Ответы стоит поместить в отдельные переменные
+// Записать ответы в объект movies в формате: 
+//     movies: {
+//         'logan': '8.1'
+//     }
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("на сколько оцените его?", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("на сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+"use strict";
+
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотели?", "");
 
-const personalMovieDB =  {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
-    
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("на сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("на сколько оцените его?", "");
-    
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// Автоматизировать вопросы пользователю про фильмы при помощи цикла
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("на сколько оцените его?", "");
+// Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+//отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+//возвращаем пользователя к вопросам опять        
+  if (a == null || a == "" || a.length > 50 || b == null || b == "" || b.length > 50) {
+    i--;
+  } else {
+    personalMovieDB.movies[a] = b;
+  }
+}
+//При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+//"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+//"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено слишком мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("произошла ошибка");
+}
+
+console.log("personalMovieDB");
